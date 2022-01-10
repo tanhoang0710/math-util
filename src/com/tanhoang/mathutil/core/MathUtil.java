f967ea68886576a883f5686c336053e8b341b20d
tanhoang0710 <tanhoang0710@gmail.com>
2022-01-10 10:10:24 +0700

    Add recursion to see GREEN 4EVER again - regression testing

--- src/com/tanhoang/mathutil/core/MathUtil.java
+++ src/com/tanhoang/mathutil/core/MathUtil.java
@@ -19,16 +19,28 @@ public class MathUtil {
     // Test First Development, suy nghi, viet ve bo test case/ cac tinh huong
     // ham truoc khi viet ham
     
-    public static long getFactorial(int n) {
-        if(n < 0 || n > 20)
-            throw new IllegalArgumentException("n must be between 0 ... 20");
+    
+    
+//    public static long getFactorial(int n) {
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("n must be between 0 ... 20");
+//        
+//        if(n == 0 || n == 1) return 1; // n dac biet
+//        
+//        long product = 1; // khoi dau gia thua la 1
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    
+    public static long getFactorial(int n){
+        if(n < 0 || n > 20) 
+            throw new IllegalArgumentException("n must be between 0..20");
         
-        if(n == 0 || n == 1) return 1; // n dac biet
+        if(n == 0 || n == 1)
+            return 1;
         
-        long product = 1; // khoi dau gia thua la 1
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
     }
 }
